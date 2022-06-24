339cb144ff5988bde638c8816a6e56f70dac9d75
Akshatgeneration <108129047+Akshatgeneration@users.noreply.github.com>
2022-06-24 16:59:52 +0530

    first commit
    This is the code for automatic irrigation system. You can watch my video how to make automatic irrigation system on my channel learn electronics and robotics on youtube

---
+++ automatic_irrigation__system.ino
@@ -0,0 +1,37 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+int msensor = A0;
+int msvalue = 0; 
+int led = 13; 
+boolean flag = false; 
+void setup() {
+  Serial.begin(9600);
+  pinMode(msensor, INPUT);
+  pinMode(led, OUTPUT);  
+}
+ 
+void loop() {
+  msvalue = analogRead(msensor);
+  Serial.println(msvalue);
+  
+  if ( (msvalue >= 500  ) && ( flag == false ) )
+{
+  digitalWrite(led, HIGH); 
+   motor1.setSpeed(225); 
+  motor1.run(FORWARD); 
+  flag = true; 
+  delay(1000); 
+}
+ 
+  if ( (msvalue <= 300  ) && ( flag == true ) )
+{
+  digitalWrite(led, LOW); 
+   motor1.setSpeed(0); 
+  motor1.run(RELEASE); 
+  flag = false; 
+  delay(1000); 
+}
+ 
+  delay(1000);   
+}   
